ad2527b69a6d921badfcbe46a07ffe77b2073899
Joel Delgado Castillo <veracubano@gmail.com>
2020-04-16 13:16:41 -0500

    Primer commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages/pages.component';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent},
+      { path: 'progress', component: ProgressComponent},
+      { path: 'graficas1', component: Graficas1Component},
+      { path: '', pathMatch: 'full' , redirectTo: '/dashboard' }
+    ]
+  },
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: '**', component: NopagefoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
